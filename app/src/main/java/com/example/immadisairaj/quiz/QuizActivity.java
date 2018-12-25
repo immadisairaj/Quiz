248881ce050c88e9470c22db405402c7d44eaaf5
Sai Rajendra Immadi <immadirajendra.sai@gmail.com>
2018-12-25 21:33:20 +0530

    Update QuizActivity.java

--- app/src/main/java/com/example/immadisairaj/quiz/QuizActivity.java
+++ app/src/main/java/com/example/immadisairaj/quiz/QuizActivity.java
@@ -275,9 +275,6 @@ public class QuizActivity extends AppCompatActivity {
         });
         AlertDialog scoreDialog = builder.create();
         scoreDialog.show();
-        Button button = findViewById(R.id.submit);
-        button.setVisibility(View.INVISIBLE);
-
     }
 
     public void clickSolutions() {
